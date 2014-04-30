4fc2ddb3812b4398047ff9f12571b27bfd3d86a6
Aron Culotta <aronwc@gmail.com>
2014-04-30 13:30:57 -0500

    first pass of scoring function

--- purpletag/purpletag_score.py
+++ purpletag/purpletag_score.py
@@ -5,13 +5,17 @@ Compute polarity scores for all .tags files that we haven't yet processed.
 Options
     -h, --help             help
     -r, --refresh-mocs     fetch latest legislator information from GovTrack
+    -c, --counts           use hashtag count features instead of binary features
 """
-
 from docopt import docopt
 import io
-import requests
 
+import numpy as np
+from scipy.sparse import csr_matrix
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_selection import chi2
+from sklearn.preprocessing import LabelEncoder
+import requests
 
 from . import config
 from data import get_basenames, get_files, parse_legislators, parse_twitter_handles
@@ -23,7 +27,23 @@ def has_score_file(tag_file, score_files_bn):
     return bn in score_files_bn
 
 
-def parse_tag_file(tag_file, handle2party):
+def parse_tags(parts, args):
+    """ Parse a line of a .tags file into a dict from tag to count.
+    >>> sorted(parse_tags(['foo:10', 'bar:5'], {'--counts':True}).items())
+    [('bar', 5.0), ('foo', 10.0)]
+    >>> sorted(parse_tags(['foo:10', 'bar:5'], {'--counts':False}).items())
+    [('bar', 1.0), ('foo', 1.0)]
+    """
+    tags = {}
+    for t in parts:
+        tag_val = t.split(':')
+        if not args['--counts']:
+            tag_val[1] = 1.
+        tags[tag_val[0]] = float(tag_val[1])
+    return tags
+
+
+def parse_tag_file(tag_file, handle2party, args):
     """ Return list of twitter handles, tag counts, and labels. """
     inf = io.open(tag_file, mode='rt', encoding='utf8')
     labels = []
@@ -32,26 +52,73 @@ def parse_tag_file(tag_file, handle2party):
     for line in inf:
         parts = line.strip().split()
         handle = parts[0]
-        tags = dict([(t.split(':')[0], t.split(':')[1]) for t in parts[1:]])
         if handle in handle2party:
             labels.append(handle2party[handle])
-            dicts.append(tags)
+            dicts.append(parse_tags(parts[1:], args))
             handles.append(handle)
     return (handles, dicts, labels)
 
 
-def score(tag_file, handle2party):
+def feat_counts(fi, ci, X, y):
+    """
+    Count the number of times feature fi occurs in a row with class ci.
+
+    >>> feat_counts(0, 0, csr_matrix([[0], [1], [0], [1]]), [0, 1, 0, 0])
+    1
+    >>> feat_counts(0, 1, csr_matrix([[0], [1], [0], [1]]), [0, 1, 0, 1])
+    2
+    """
+    indices = [idx for idx, val in enumerate(y) if val == ci]
+    return X[indices][:, fi].sum()
+
+
+def feat_class_pr(X, y, ci, V):
+   return (1. + np.array([feat_counts(i, ci, X, y) for i in range(V)])) \
+       / (V + len([yi for yi in y if yi == ci]))
+
+
+def compute_signs(X, y, V):
+    """ For each feature, return 1 if correlated with class 1, else return -1.
+    >>> compute_signs(csr_matrix([[1, 0], [1, 0], [0, 1], [0, 1]]), [1, 1, 0, 0], 2)
+    [1.0, -1.0]
+    """
+    pr0 = feat_class_pr(X, y, 0, V)
+    pr1 = feat_class_pr(X, y, 1, V)
+    return [1. if p1 > p0 else -1. for p0, p1 in zip(pr0, pr1)]
+
+
+def score_features(X, y, V, args):
+    """ FIXME: allow different options. """
+    chis, pvals = chi2(X, y)
+    signs = compute_signs(X, y, V)
+    return np.multiply(chis, signs)
+
+
+def write_scores(scores, vocab, tag_file):
+    basename = get_basenames([tag_file])[0]
+    fp = io.open(config.get('data',
+                            'path') + '/' + config.get('data', 'scores') + '/' + basename + '.scores',
+                 mode='wt', encoding='utf8')
+    for word, score in sorted(zip(vocab, scores), key=lambda x: x[1]):
+        fp.write('%s %g\n' % (word, score))
+    fp.close()
+
+
+def score(tag_file, handle2party, args):
     """ Output a file with hashtag scores. """
-    handles, tags, parties = parse_tag_file(tag_file, handle2party)
+    handles, tags, parties = parse_tag_file(tag_file, handle2party, args)
     if len(tags) == 0:
         raise ValueError("can't find any legislators in .tags files. Perhaps the legislator yaml file is bad?")
 
-    print tags[0]
     vec = DictVectorizer()
     X = vec.fit_transform(tags)
-    print X[0]
-    print vec.vocabulary_
-    # FIXME: Finish this by scoring each hashtag.
+    print '10 features=', vec.vocabulary_.items()[:10]
+    label_enc = LabelEncoder()
+    y = label_enc.fit_transform(parties)
+    print 'vectorized %d instances', len(y)
+    print 'classes=', label_enc.classes_
+    scores = score_features(X, y, len(vec.vocabulary_), args)
+    write_scores(scores, vec.get_feature_names(), tag_file)
 
 
 def get_tag_files():
@@ -75,7 +142,7 @@ def twitter_handle_to_party():
     FIXME: This assumes bioguide ids always exist. We also assume that the party of your last term is your party.
     """
     twitter_handles = parse_twitter_handles()
-    bioguide2handle = dict([(t['id']['bioguide'], t['social']['twitter']) for t in twitter_handles])
+    bioguide2handle = dict([(t['id']['bioguide'], t['social']['twitter'].lower()) for t in twitter_handles])
     legislators = parse_legislators()
     handle2party = dict()
     for leg in legislators:
@@ -88,16 +155,17 @@ def twitter_handle_to_party():
 
 def main():
     args = docopt(__doc__)
-    print args
     if args['--refresh-mocs']:
         print 'refreshing MOCs'
         fetch_legislators()
+    print 'reading twitter handles'
     handle2party = twitter_handle_to_party()
     print handle2party.items()[0]
     tag_files = get_tag_files()
     for tag_file in tag_files:
         print 'scoring', tag_file
-        score(tag_file, handle2party)
+        score(tag_file, handle2party, args)
+
 
 if __name__ == '__main__':
     main()
