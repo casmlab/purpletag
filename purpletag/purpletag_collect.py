5ea5679e1a3ba36947d68e9820fbd2218dd268ff
Aron Culotta <aronwc@gmail.com>
2014-05-01 14:26:57 -0500

    fix collection routines (for now); put all twitter collection routines in tapilab/twutil repo. closes #5; closes #6

--- purpletag/purpletag_collect.py
+++ purpletag/purpletag_collect.py
@@ -15,7 +15,7 @@ import json
 import time
 import requests
 
-from TwitterAPI import TwitterAPI
+import twutil
 
 from . import config
 from data import parse_twitter_handles
@@ -34,61 +34,34 @@ def make_output_file():
     return config.get('data', 'path') + '/' + config.get('data', 'jsons') + '/' + str(int(time.time())) + '.json'
 
 
-def track_users(ids, api):
+def track_users(ids):
     # FIXME: broken
-    raise NotImplementedError('track_users not yet implemented.')
     print 'tracking', len(ids), 'users'
     outf = io.open(make_output_file(), mode='wt', encoding='utf8')
-    results = api.request('statuses/filter', {'follow': ','.join(ids)})
-    print results.text
     count = 0
-    for tweet in results.get_iterator():
-        outf.write(json.dumps(tweet))
-        outf.write('\n')
+    for tweet in twutil.collect.track_user_ids(ids):
+        outf.write(json.dumps(tweet, ensure_ascii=False, encoding='utf8'))
+        outf.write(u'\n')
+        outf.flush()
         count += 1
         if count > 1000:
             outf.close()
             outf = make_output_file()
             count = 0
-
-
-def search_users(ids, api):
-    """ Use the Twitter REST API to fetch the most recent 3,200 tweets from
-    each user in the list of ids. When rate limits are encountered, sleep for
-    5 minutes and try again."""
-    print 'searching for', len(ids), 'users'
-    outf = io.open(make_output_file(), mode='w', encoding='utf8')
-    for id_ in ids:
-        max_id = None
-        print 'searching for', id_
-        while True:
-            if max_id:
-                results = api.request('statuses/user_timeline', {'id': id_, 'count': 200, 'max_id': max_id})
-            else:
-                results = api.request('statuses/user_timeline', {'id': id_, 'count': 200})
-            print '\tstatus:', results.status_code
-            tweets = [t for t in results]
-            if results.status_code != 200:  # something went wrong
-                print 'sleeping on error:', results.text
-                time.sleep(300)
-            elif len(tweets) == 0:  # on to the next user.
-                break
-            else:  # save these tweets and update max_id
-                outf.write(results.text + '\n')
-                max_id = min(t['id'] for t in results) - 1
     outf.close()
 
 
-def lookup_ids(handles, api):
-    """ Fetch the twitter ids of each screen_name. """
-    ids = set()
-    for handle_list in [handles[100 * i:100 * i + 100] for i in range(len(handles))]:
-        if len(handle_list) > 0:
-            print handle_list
-            r = api.request('users/lookup', {'screen_name': ','.join(handle_list)})
-            for item in r.get_iterator():
-                ids.add(item['id_str'])
-    return ids
+def search_users(screen_names):
+    """ Use the Twitter REST API to fetch the most recent 3,200 tweets from
+    each user in the list of ids. When rate limits are encountered, sleep for
+    5 minutes and try again."""
+    print 'searching for', len(screen_names), 'users'
+    outf = io.open(make_output_file(), mode='w', encoding='utf8')
+    for screen_name in screen_names:
+        print 'searching for', screen_name
+        for tweet in twutil.collect.tweets_for_user(screen_name):
+            outf.write(json.dumps(tweet, ensure_ascii=False, encoding='utf8') + '\n')
+    outf.close()
 
 
 def main():
@@ -99,15 +72,11 @@ def main():
     handle_yaml = parse_twitter_handles()
     handles = [d['social']['twitter'] for d in handle_yaml]
     print 'read', len(handles), 'handles'
-    api = TwitterAPI(config.get('twitter', 'consumer_key'),
-                     config.get('twitter', 'consumer_secret'),
-                     config.get('twitter', 'access_token'),
-                     config.get('twitter', 'access_token_secret'))
-    ids = lookup_ids(handles, api)
     if args['--track']:
-        track_users(ids, api)
+        ids = twutil.collect.lookup_ids(handles)
+        track_users(ids)
     elif args['--search']:
-        search_users(ids, api)
+        search_users(handles)
 
 
 if __name__ == '__main__':
